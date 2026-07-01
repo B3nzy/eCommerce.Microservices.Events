0e00ac86d3813194db85da3e136d010a9ae14e04
Sumit Mandal <sumit.mandal0123@gmail.com>
2026-07-01 19:40:52 +0530

    Add project files.

---
+++ eCommerce.Microservices.Events/Order/OrderPlaced.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.Microservices.Events.Order;
+
+public class OrderPlaced
+{
+    public Guid OrderId { get; set; }
+    public Guid UserId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public decimal TotalBill { get; set; }
+    public List<OrderItem>? OrderItems { get; set; }
+}
+
+public class OrderItem
+{
+    public Guid ProductId { get; set; }
+    public decimal UnitPrice { get; set; }
+    public int Quantity { get; set; }
+    public decimal TotalPrice { get; set; }
+}
