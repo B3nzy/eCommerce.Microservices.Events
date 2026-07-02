a1ab43cc7e3e6aa968c83de44794a00da38a2bd4
Sumit Mandal <sumit.mandal0123@gmail.com>
2026-07-02 20:13:15 +0530

    Add ProductStock Event

---
+++ eCommerce.Microservices.Events/Product/ProductStock.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.Microservices.Events.Product;
+
+public class ProductStock
+{
+    public Guid ProductId { get; set; }
+    public int QuantityInStock { get; set; }
+}
